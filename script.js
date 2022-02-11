b06bc2170c2e37c08dabf52a9f5d728187953e50
jenboc <47900748+jenboc@users.noreply.github.com>
2022-02-11 17:55:25 +0000

    Updating Style
    Updating style changed to use properties and setters rather than directly changing the cssText in a seperate function

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 words = ["tick", "tock"]
 
 class Object {
-	constructor(objId="none", rotateDeg=0, innerText="none", fgcolor="#000000", classes="", posLeft="none", posTop="none") {
+	constructor(objId="none", rotateDeg=0, innerText="none", fgcolor="#000000", bgcolor="#ffffff", classes="", posLeft="none", posTop="none") {
 		if (objId == "none") {
 			this.object = document.createElement("div")
-			this.object.onclick = this.delete
 		}
 		else {
 			this.object = document.getElementById(objId)
 		}
 
-		if (innerText != "none") {
-			this.object.innerHTML = innerText
-		}
+		this.innerText = innerText
 
 		this.fgcolor = fgcolor
 		this.rotation = rotateDeg
@@ -20,36 +17,40 @@ class Object {
 		this.posTop = posTop
 
 		this.object.className = classes
-
-		this.updateStyle()
 	}
 
-	updateStyle() {
-		this.object.style.color = this.fgcolor
-		this.object.style.backgroundColor = this.bgcolor
-		
-		if (this.rotation > 0) {
-			this.object.style.transform += "rotate(" + this.rotation + "deg)"
-		}
+	set fgcolor(newColor) {
+		this.object.style.color = newColor
+	}
 
-		if (this.posLeft != "none" && this.posTop != "none") {
-			this.object.style.top = this.posTop + "%"
-			this.object.style.left = this.posLeft + "%"
-		}
+	set bgcolor(newColor) {
+		this.object.style.backgroundColor = newColor
+	}
+
+	set rotation(newRotation) {
+		this.object.style.transform = "rotate(" + newRotation + "deg)"
+	}
+
+	set posLeft(newPos) { 
+		this.object.style.left = newPos + "%"
+	}
+
+	set posTop(newPos) {
+		this.object.style.top = newPos + "%"
+	}
+
+	set innerText(text) {
+		this.object.innerText = text
 	}
 
 	setParent(parentId) {
 		document.getElementById(parentId).appendChild(this.object)
 	}
-
-	delete(){
-		this.object.remove()
-	}
 }
 
 class Clock extends Object {
 	constructor() {
-		super("time")
+		super("time", 0, "HELLO")
 	}
 
 	updateTime() {		
@@ -74,12 +75,8 @@ class Clock extends Object {
 	}
 
 	changeColor(){
-		do {
-			this.bgcolor = RandColor();
-			this.fgcolor = RandColor();
-		} while (this.bgcolor == this.fgcolor)
-
-		this.updateStyle()
+		this.bgcolor = RandColor();
+		this.fgcolor = RandColor();
 	}
 }
 
@@ -106,39 +103,33 @@ function RandColor(){
 
 
 function AddRandom(){
-	var newTickTock = new Object(objId="none", rotateDeg=randNumber(360), innerText=words[randNumber(words.length)], fgcolor=RandColor(), classes='random-word', posLeft=randNumber(100), posTop=randNumber(100))
+	var newTickTock = new Object(objId="none", rotateDeg=randNumber(360), innerText=words[randNumber(words.length)], fgcolor=RandColor(), bgcolor="#ffffff", classes='random-word', posLeft=randNumber(100), posTop=randNumber(100))
 	newTickTock.setParent('random-container')
-	localStorage.ticktock = document.getElementById('random-container')
 }
-
 
 function cchangecolor(){
 	clock.changeColor()
 }
 
+function GetSettings() {
+	var query = window.location.search;
+	var parameters = new URLSearchParams(query);
 
-container = document.getElementById('random-container')
-container.innerHTML = localStorage.ticktock
+	var settings = [500, 1000]
 
-var query = window.location.search;
-var parameters = new URLSearchParams(query);
+	if (parameters.has('cchange')){
+		cchange = parameters.get('cchange')
+	}
 
-var cchange = 0;
-if (parameters.has('cchange')){
-	cchange = parameters.get('cchange')
-}
-else{
-	cchange = 500
+	if (parameters.has('tadd')){
+		tadd = parameters.get('tadd')
+	}
+
+	return settings
 }
 
-var tadd = 0;
-if (parameters.has('tadd')){
-	tadd = parameters.get('tadd')
-}
-else {
-	tadd = 1000
-}
+settings = GetSettings()
 
 window.setInterval(clock.updateTime, 1000);
-window.setInterval(cchangecolor,cchange)
-window.setInterval(AddRandom, tadd);
+window.setInterval(cchangecolor,settings[0])
+window.setInterval(AddRandom, settings[1]);
